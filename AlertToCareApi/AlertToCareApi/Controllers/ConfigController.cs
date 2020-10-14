27eacbd337d9ae5ca3c4bfc1088458154cd6b3db
Rajshree Rathour <Rajshree.Rathour@philips.com>
2020-10-14 12:21:59 +0530

    First Commit

---
+++ AlertToCareApi/AlertToCareApi/Controllers/ConfigController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AlertToCareApi.Models;
+
+namespace AlertToCareApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        Beds[] beds = new Beds[]
+        {
+            new Beds{ id = 1,occupancyStatus= true },
+            new Beds{ id = 2,occupancyStatus= false },
+        };
+
+        [HttpGet]
+        public IEnumerable<Beds> Get()
+        {
+            return beds;
+        }
+    }
+}
---
+++ AlertToCareApi/AlertToCareApi/Models/Beds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlertToCareApi.Models
+{
+    public class Beds
+    {
+        public int id { get; set; }
+        public bool occupancyStatus { get; set; }
+    }
+}
